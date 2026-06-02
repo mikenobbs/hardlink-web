35cedc6f087f5fe0a17a16b331bc86495c9dfcae
mikenobbs <45309438+mikenobbs@users.noreply.github.com>
2026-06-02 16:26:43 +0100

    Implement secret key generation in entrypoint.sh
    Add secret key generation if not present

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,9 @@ if [ ! -f /config/config.yml ]; then
     cp /app/config.yml /config/config.yml
 fi
 
+if [ ! -f /config/.secret_key ]; then
+    echo "Generating secret key..."
+    python -c "import secrets; open('/config/.secret_key', 'w').write(secrets.token_hex(32))"
+fi
+
 exec gunicorn --bind 0.0.0.0:8088 --workers 2 --chdir /app app:app
