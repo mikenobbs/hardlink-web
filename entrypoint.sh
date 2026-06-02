4e21c0842075822df0db0f940abcb6b0144209a6
mikenobbs <45309438+mikenobbs@users.noreply.github.com>
2026-06-02 13:36:37 +0100

    fixed entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ if [ ! -f /config/config.yml ]; then
     cp /app/config.yml /config/config.yml
 fi
 
-exec gunicorn --bind 0.0.0.0:8088 --workers 2 app:app
+exec gunicorn --bind 0.0.0.0:8088 --workers 2 --chdir /app app:app
